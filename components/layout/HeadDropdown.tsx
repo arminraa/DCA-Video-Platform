3e3e5c62e5e8c8d47a438413c5fe8b6ff0bcc0bb
Armin Rahbar <arminmuch41@gmail.com>
2024-09-18 16:37:18 +0330

    Head Dropdown master

---
+++ components/layout/HeadDropdown.tsx
@@ -0,0 +1,11 @@
+export default function HeadDropdown({
+  display,
+}: Readonly<{
+  display: "hidden" | "block";
+}>) {
+  return (
+    <div
+      className={`${display} absolute md:max-w-[676px] lg:max-w-[1000px] w-full h-[300px] md:h-[400px] shadow-2xl z-30 bg-gray-400 top-[120px] sm:top-[90px] md:right-2 xl:right-40 right-0 rounded-lg`}
+    ></div>
+  );
+}
